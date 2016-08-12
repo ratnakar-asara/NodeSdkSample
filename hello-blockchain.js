06aa22187d9b10e7cb0f1dc41120e68ee0593c80
ratnakar <rasara@us.ibm.com>
2016-08-12 16:27:20 -0400

    Initial commit

---
+++ hello-blockchain.js
@@ -0,0 +1,131 @@
+// Include the package from npm:
+//var hfc = require('hfc');
+var hfc = require('../..');
+var util = require('util');
+var fs = require('fs');
+
+// Create a client chain.
+var chain = hfc.newChain("targetChain");
+
+// Configure the KeyValStore which is used to store sensitive keys
+// as so it is important to secure this storage.
+// do a mkdir ~/tmp  (dont check if fails..just so it is there
+chain.setKeyValStore(hfc.newFileKeyValStore('/tmp/keyValStore'));
+
+chain.setMemberServicesUrl("grpc://localhost:50051");
+var peer = chain.addPeer("grpc://localhost:30303");
+
+var testChaincodeID;
+
+// Enroll "admin" which is already registered because it is
+// listed in fabric/membersrvc/membersrvc.yaml with it's one time password.
+chain.enroll("test_user0", "MS9qrN8hFjlE", function(err, user1) {
+    if (err) return console.log(util.format("ERROR: failed to register test_user0, Error : %j \n", err));
+    // Set this user as the chain's registrar which is authorized to register other users.
+    chain.setRegistrar(user1);
+
+    console.log("\nEnrolled test_user0 successfully\n");
+
+    var userName = "test_user1";
+    chain.enroll(userName, "jGlNl6ImkuDo", function(err, user2) {
+        if (err) return console.log(util.format("ERROR: failed to register test_user1 , Error : %s\n", err));
+
+        // registrationRequest
+        var registrationRequest = {
+            enrollmentID: userName,
+            account: "group1",
+            affiliation: "00001"
+        };
+        chain.registerAndEnroll(registrationRequest, function(err, user) {
+            if (err) throw Error(" Failed to register and enroll " + userName + ": " + err);
+
+            console.log("Enrolled test_user2 successfully\n");
+
+            chain.setDeployWaitTime(60);
+            //chain.setInvokeWaitTime(10);
+            deployChaincode(user);
+        });
+    });
+});
+
+function deployChaincode(user) {
+    console.log(util.format("Deploying chaincode ... It will take about %j seconds to deploy \n", chain.getDeployWaitTime()))
+        // Construct the deploy request
+    var deployRequest = {
+        chaincodePath: "github.com/chaincode_example02/",
+        // Function to trigger
+        fcn: "init",
+        // Arguments to the initializing function
+        args: ["a", "100", "b", "200"]
+    };
+
+    // Trigger the deploy transaction
+    var deployTx = user.deploy(deployRequest);
+
+    // Print the deploy results
+    deployTx.on('complete', function(results) {
+        // Deploy request completed successfully
+        testChaincodeID = results.chaincodeID;
+        console.log(util.format("[ Chaincode ID : ", testChaincodeID+" ]\n"));
+        console.log(util.format("Successfully deployed chaincode: request=%j, response=%j \n", deployRequest, results));
+
+        invokeOnUser(user);
+    });
+    deployTx.on('error', function(err) {
+        // Deploy request failed
+        console.log(util.format("Failed to deploy chaincode: request=%j, error=%j \n", deployRequest, err));
+    });
+}
+
+function invokeOnUser(user) {
+    // Construct the invoke request
+    var invokeRequest = {
+        // Name (hash) required for invoke
+        chaincodeID: testChaincodeID,
+        // Function to trigger
+        fcn: "invoke",
+        // Parameters for the invoke function
+        args: ["a", "b", "1"]
+    };
+
+    // Trigger the invoke transaction
+    var invokeTx = user.invoke(invokeRequest);
+
+    invokeTx.on('submitted', function(results) {
+        // Invoke transaction submitted successfully
+        console.log(util.format("submitted chaincode invoke transaction: request=%j, response=%j\n", invokeRequest, results));
+    });
+    invokeTx.on('complete', function(results) {
+        // Invoke transaction completed?
+        console.log(util.format("completed chaincode invoke transaction: request=%j, response=%j\n", invokeRequest, results));
+        queryUser(user);
+    });
+    invokeTx.on('error', function(err) {
+        // Invoke transaction submission failed
+        console.log(util.format("Failed to submit chaincode invoke transaction: request=%j, error=%j\n", invokeRequest, err));
+    });
+}
+
+function queryUser(user) {
+    // Construct the query request
+    var queryRequest = {
+        // Name (hash) required for query
+        chaincodeID: testChaincodeID,
+        // Function to trigger
+        fcn: "query",
+        // Existing state variable to retrieve
+        args: ["a"]
+    };
+
+    // Trigger the query transaction
+    var queryTx = user.query(queryRequest);
+
+    queryTx.on('complete', function(results) {
+        // Query completed successfully
+        console.log("Successfully queried  chaincode function: request=%j, value=%s \n", queryRequest, results.result.toString());
+    });
+    queryTx.on('error', function(err) {
+        // Query failed
+        console.log("Failed to query chaincode, function: request=%j, error=%j \n", queryRequest, err);
+    });
+}
